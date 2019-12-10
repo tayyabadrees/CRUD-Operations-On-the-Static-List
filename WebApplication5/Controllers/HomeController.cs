fce6d9f0ef43f8faf8d58d353cbe25c521dc3a7b
tayyabadrees <39157259+tayyabadrees@users.noreply.github.com>
2019-12-10 06:55:04 +0500

    Add project files.

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<Course> _courses = new List<Course>()
+        { new Course() { Title = "physics", CourseCode = "bse21 ",},
+          new Course() { Title = "Math", CourseCode = "bse26" ,},
+        };
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult GetCourses()
+        { return Json(_courses); }
+        [HttpPost]
+        public JsonResult AddCourse(Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                _courses.Add(course);
+            }
+            return Json("");
+        }
+      
+        
+        [HttpPost]
+        public JsonResult Delete(string courseCode)
+        {
+         Course  c=_courses.ToList().Find(x => x.CourseCode.Equals(courseCode));
+            _courses.Remove(c);
+            return Json(courseCode);
+        }
+
+       
+        [HttpPost]
+        public JsonResult Update(Course course)
+        {
+            if (ModelState.IsValid)
+            { 
+                Course c = _courses.ToList().Find(x => x.CourseCode.Equals(course.CourseCode));
+                c.Title = course.Title;
+            }
+            return Json("successful");
+         
+        }
+        public JsonResult getById(string courseCode)
+        {
+            return Json(_courses.ToList().Find(x => x.CourseCode.Equals(courseCode)), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ WebApplication5/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace WebApplication5.Models
+{
+    public class Course
+    {
+
+        //[Required]
+        //[MaxLength(100)]
+        public string Title { get; set; }
+            public string CourseCode { get; set; }
+            
+        
+            
+
+    }
+}
